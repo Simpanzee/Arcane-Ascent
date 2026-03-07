a24b8b6f7df32464e05cfe62a85ea78700aa4350
TwilightEch03s <lorenzokiri.uk@gmail.com>
2026-03-07 12:35:10 -0800

    Reorganization

--- Scripts/Screens/main_menu.gd
+++ Scripts/Screens/main_menu.gd
@@ -5,21 +5,21 @@ extends Node2D
 @onready var hover = $Hover
 
 
-func _ready() -> void:
-	menu_theme.play()
+#func _ready() -> void:
+	#menu_theme.play()
 
 func _on_play_pressed() -> void:
 	click.play()
 	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://game.tscn")
 	
-func _on_quit_pressed() -> void:
-	click.play()
-	await get_tree().create_timer(0.5).timeout
-	get_tree().quit()
-
 func _on_play_mouse_entered() -> void:
 	hover.play()
+
+func _on_quit_pressed() -> void:
+	click.play()
+	await get_tree().create_timer(0.2).timeout
+	get_tree().quit()
 
 func _on_quit_mouse_entered() -> void:
 	hover.play()
