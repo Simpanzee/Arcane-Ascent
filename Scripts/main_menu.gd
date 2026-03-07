4b6746c59cdf45d935a25dfd8230e743321eb0d6
TwilightEch03s <lorenzokiri.uk@gmail.com>
2026-03-07 03:56:11 -0800

    Menu

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,12 +1,25 @@
 extends Node2D
 
 @onready var menu_theme = $MenuTheme
+@onready var click = $Click
+@onready var hover = $Hover
+
 
 func _ready() -> void:
 	menu_theme.play()
 
 func _on_play_pressed() -> void:
+	click.play()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://game.tscn")
 	
 func _on_quit_pressed() -> void:
+	click.play()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().quit()
+
+func _on_play_mouse_entered() -> void:
+	hover.play()
+
+func _on_quit_mouse_entered() -> void:
+	hover.play()
