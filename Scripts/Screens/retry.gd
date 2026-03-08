48692cca3e7236e3beb53d8301c504e1b0b4bfba
TwilightEch03s <lorenzokiri.uk@gmail.com>
2026-03-08 05:40:13 -0700

    Retry

--- Scripts/Screens/retry.gd
+++ Scripts/Screens/retry.gd
@@ -1,7 +1,20 @@
 extends Node2D
 
+@onready var click = $Click
+@onready var hover = $Hover
+
 func _on_retry_pressed() -> void:
+	click.play()
+	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://game.tscn")
 
 func _on_menu_pressed() -> void:
+	click.play()
+	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+func _on_retry_mouse_entered() -> void:
+	hover.play()
+
+func _on_menu_mouse_entered() -> void:
+	hover.play()
