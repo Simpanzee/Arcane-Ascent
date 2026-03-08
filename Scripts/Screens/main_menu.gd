c756b7b41b9b180e747889f854e463bb4234c500
TwilightEch03s <lorenzokiri.uk@gmail.com>
2026-03-08 03:12:01 -0700

    New Menu

--- Scripts/Screens/main_menu.gd
+++ Scripts/Screens/main_menu.gd
@@ -3,23 +3,63 @@ extends Node2D
 @onready var menu_theme = $MenuTheme
 @onready var click = $Click
 @onready var hover = $Hover
+@onready var walk = $Walk
 
+@onready var sprite = $AnimatedSprite2D
 
-#func _ready() -> void:
-	#menu_theme.play()
+@onready var play_button = $ButtonManager/Play
+@onready var quit_button = $ButtonManager/Quit
+@onready var settings_button = $ButtonManager/Settings
+
+var running := false
+var run_speed := 300
+var leaving := false
+
+func _ready() -> void:
+	sprite.play("default")
+	menu_theme.play()
+
+func _process(delta):
+	if running:
+		sprite.position.x -= run_speed * delta
+		
+		if sprite.position.x < -100 and not leaving:
+			leave_menu()
+
+func leave_menu() -> void:
+	leaving = true
+	running = false
+	var fade_time := 2.0
+	var steps := 5
+	for i in steps:
+		walk.volume_db = lerp(0.0, -40.0, float(i) / steps)
+		await get_tree().create_timer(fade_time / steps).timeout
+	
+	walk.stop()
+	await get_tree().create_timer(1.0).timeout
+	get_tree().change_scene_to_file("res://game.tscn")
 
 func _on_play_pressed() -> void:
 	click.play()
-	await get_tree().create_timer(0.5).timeout
-	get_tree().change_scene_to_file("res://game.tscn")
-	
+	menu_theme.stop()
+	sprite.play("run")
+	walk.play()
+	running = true
+	$ButtonManager.hide()
+	$Title.hide()
+	await get_tree().create_timer(0.6).timeout
+	$Staff.hide()
+
 func _on_play_mouse_entered() -> void:
 	hover.play()
 
 func _on_quit_pressed() -> void:
 	click.play()
-	await get_tree().create_timer(0.2).timeout
 	get_tree().quit()
 
 func _on_quit_mouse_entered() -> void:
 	hover.play()
+
+func _on_settings_pressed() -> void:
+	click.play()
+	#await get_tree().create_timer(0.2).timeout
